6dfea2966708400f7720d7320673531e2bfa77a1
Yoon Yeoung-jin <alwns28@kookmin.ac.kr>
2022-12-11 15:37:11 +0900

    [code] table view button and change data function develop
    * 테이블 뷰 데이터 변경 버튼 추가
    * 테이블 뷰 데이터 변경 로직 추가

--- BlueLibrarySwift_YYJ/BlueLibrarySwift_YYJ/Controller/ViewController.swift
+++ BlueLibrarySwift_YYJ/BlueLibrarySwift_YYJ/Controller/ViewController.swift
@@ -13,15 +13,29 @@ class ViewController: UIViewController {
     
     var albumScrollView: UIScrollView!
     
+    /* scroll View frame Setting */
     fileprivate var scrollWidth: CGFloat = 280
     fileprivate let scrollHight: CGFloat = 150
     fileprivate let thumbNailWidth: CGFloat = 120
     fileprivate let thumbNailHeight: CGFloat = 120
     fileprivate let padding: CGFloat = 10
     
+    /* tableView cell Data */
+    fileprivate var tableViewArtistData: String = ""
+    fileprivate var tableViewAlbumData: String = ""
+    fileprivate var tableViewGenreData: String = ""
+    fileprivate var tableViewYearData: String = ""
+    
     var albumDataList: [MusicInfo] = []
     
     @IBOutlet weak var albumTableView: UITableView!
+    
+    fileprivate func loadTableViewDataSetting(_ index: Int) {
+        self.tableViewArtistData = albumDataList[index].Artist
+        self.tableViewAlbumData = albumDataList[index].Album
+        self.tableViewGenreData = albumDataList[index].Genre
+        self.tableViewYearData = String(albumDataList[index].Year)
+    }
     
     fileprivate func loadTableView() {
         self.albumTableView.delegate = self
@@ -29,29 +43,52 @@ class ViewController: UIViewController {
     }
     
     fileprivate func makeScrollView() {
-        let imageStrings: [String] = ["image01","image02","image03","image04","image05","image06","image07"]
         scrollWidth = self.view.frame.width
         
         // scroll view setting
         albumScrollView = UIScrollView(frame: CGRectMake(0, 0 + scrollHight - 40, scrollWidth, scrollHight))
         
-        let contentSizeWidth:CGFloat = (thumbNailWidth + padding) * (CGFloat(imageStrings.count))
+        let contentSizeWidth:CGFloat = (thumbNailWidth + padding) * (CGFloat(albumDataList.count))
         let contentSize = CGSize(width: contentSizeWidth, height: thumbNailHeight)
         
         albumScrollView.contentSize = contentSize
         albumScrollView.autoresizingMask = UIView.AutoresizingMask.flexibleWidth
         
-        for (index, value) in imageStrings.enumerated() {
+        for (index, value) in albumDataList.enumerated() {
             let button:UIButton = UIButton(type: .custom)
-            var xButton = CGFloat(padding * (CGFloat(index) + 1) + (CGFloat(index) * thumbNailWidth))
+            let xButton = CGFloat(padding * (CGFloat(index) + 1) + (CGFloat(index) * thumbNailWidth))
             button.frame = CGRectMake(xButton, padding, thumbNailWidth, thumbNailHeight)
             button.tag = index
             
-            let image = UIImage(named: value)
-            button.setBackgroundImage(image, for: .normal)
-            albumScrollView.addSubview(button)
+            
+            let url = URL(string: value.ImageUrl)
+            DispatchQueue.global().async {
+                if let data = try? Data(contentsOf: url!) {
+                    let image = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        button.setBackgroundImage(image, for: .normal)
+                        self.albumScrollView.addSubview(button)
+                        button.addTarget(self, action: #selector(self.changeTableViewInfo(sender:)), for: .touchUpInside)
+                        
+                    }
+                } else {
+                    let image = UIImage(named: "noAlbum")
+                    DispatchQueue.main.async {
+                        button.setBackgroundImage(image, for: .normal)
+                        self.albumScrollView.addSubview(button)
+                        button.addTarget(self, action: #selector(self.changeTableViewInfo(sender:)), for: .touchUpInside)
+                    }
+                }
+            }
+
         }
         self.view.addSubview(albumScrollView)
+    }
+    
+    @objc func changeTableViewInfo(sender:UIButton) {
+        // sender.tag : 해당 엘범의 index 값
+        loadTableViewDataSetting(sender.tag)
+        self.albumTableView.reloadData()
     }
 
     override func viewDidLoad() {
@@ -59,6 +96,7 @@ class ViewController: UIViewController {
         self.title = "BlueLibrary YYJ"
         self.albumDataList = loadJson(jsonFileName, fileExtention)
         loadTableView()
+        loadTableViewDataSetting(0)
         makeScrollView()
     }
 }
@@ -73,24 +111,22 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         /* Sample */
         let cell = tableView.dequeueReusableCell(withIdentifier: "albumInfoTableViewCell", for: indexPath) as! albumInfoTableViewCell
         
-        print(self.albumDataList)
-        
         switch indexPath.row {
         case 0:
             cell.infoTitle.text = "Artist"
-            cell.infoDetail.text = self.albumDataList[0].Artist
+            cell.infoDetail.text = self.tableViewArtistData
             break
         case 1:
             cell.infoTitle.text = "Album"
-            cell.infoDetail.text = self.albumDataList[0].Album
+            cell.infoDetail.text = self.tableViewAlbumData
             break
         case 2:
             cell.infoTitle.text = "Genre"
-            cell.infoDetail.text = self.albumDataList[0].Genre
+            cell.infoDetail.text = self.tableViewGenreData
             break
         case 3:
             cell.infoTitle.text = "Year"
-            cell.infoDetail.text = String(self.albumDataList[0].Year)
+            cell.infoDetail.text = self.tableViewYearData
             break
         default:
             break
--- BlueLibrarySwift_YYJ/BlueLibrarySwift_YYJ/Model/model.swift
+++ BlueLibrarySwift_YYJ/BlueLibrarySwift_YYJ/Model/model.swift
@@ -5,6 +5,12 @@
 //  Created by yoon-yeoungjin on 2022/12/06.
 //
 
+enum imageLoadType {
+    case noUrl
+    case loadingUrl
+    case finishUrl
+}
+
 /**
  해당 프로젝트의 데이터 구조체 입니다.
  */
@@ -14,6 +20,7 @@ struct MusicInfo {
     let Genre: String
     let Year: Int
     let ImageUrl: String
+    var ImageLoadType: imageLoadType
     
     init(Artist: String, Album: String, Genre: String, Year: Int, ImageUrl: String) {
         self.Artist = Artist
@@ -21,10 +28,10 @@ struct MusicInfo {
         self.Genre = Genre
         self.Year = Year
         self.ImageUrl = ImageUrl
+        self.ImageLoadType = .loadingUrl
     }
-    
+
     func elementCount() -> Int{
         return 4
     }
 }
-
