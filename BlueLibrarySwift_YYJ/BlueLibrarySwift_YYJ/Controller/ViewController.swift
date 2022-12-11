d9443e4b8f101358cc255dc0e5bca4a30762b72c
Yoon Yeoung-jin <alwns28@kookmin.ac.kr>
2022-12-11 11:04:06 +0900

    [code] scrollView Frame make

--- BlueLibrarySwift_YYJ/BlueLibrarySwift_YYJ/Controller/ViewController.swift
+++ BlueLibrarySwift_YYJ/BlueLibrarySwift_YYJ/Controller/ViewController.swift
@@ -11,6 +11,14 @@ class ViewController: UIViewController {
     fileprivate let jsonFileName:String = "albums"
     fileprivate let fileExtention: String = "json"
     
+    var albumScrollView: UIScrollView!
+    
+    fileprivate var scrollWidth: CGFloat = 280
+    fileprivate let scrollHight: CGFloat = 150
+    fileprivate let thumbNailWidth: CGFloat = 120
+    fileprivate let thumbNailHeight: CGFloat = 120
+    fileprivate let padding: CGFloat = 10
+    
     var albumDataList: [MusicInfo] = []
     
     @IBOutlet weak var albumTableView: UITableView!
@@ -19,11 +27,39 @@ class ViewController: UIViewController {
         self.albumTableView.delegate = self
         self.albumTableView.dataSource = self
     }
+    
+    fileprivate func makeScrollView() {
+        let imageStrings: [String] = ["image01","image02","image03","image04","image05","image06","image07"]
+        scrollWidth = self.view.frame.width
+        
+        // scroll view setting
+        albumScrollView = UIScrollView(frame: CGRectMake(0, 0 + scrollHight - 40, scrollWidth, scrollHight))
+        
+        let contentSizeWidth:CGFloat = (thumbNailWidth + padding) * (CGFloat(imageStrings.count))
+        let contentSize = CGSize(width: contentSizeWidth, height: thumbNailHeight)
+        
+        albumScrollView.contentSize = contentSize
+        albumScrollView.autoresizingMask = UIView.AutoresizingMask.flexibleWidth
+        
+        for (index, value) in imageStrings.enumerated() {
+            let button:UIButton = UIButton(type: .custom)
+            var xButton = CGFloat(padding * (CGFloat(index) + 1) + (CGFloat(index) * thumbNailWidth))
+            button.frame = CGRectMake(xButton, padding, thumbNailWidth, thumbNailHeight)
+            button.tag = index
+            
+            let image = UIImage(named: value)
+            button.setBackgroundImage(image, for: .normal)
+            albumScrollView.addSubview(button)
+        }
+        self.view.addSubview(albumScrollView)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "BlueLibrary YYJ"
         self.albumDataList = loadJson(jsonFileName, fileExtention)
         loadTableView()
+        makeScrollView()
     }
 }
 
@@ -31,7 +67,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return albumDataList[0].elementCount()
     }
-    
+
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         /* Sample */
